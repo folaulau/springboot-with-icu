088b3c02b634f0b8d71d6854438622b69ee4f388
Folau Kaveinga <folaukaveinga@gmail.com>
2024-01-04 11:02:28 -0800

    setup

---
+++ src/main/java/com/folautech/icu/SpringbootWithIcuApplication.java
@@ -0,0 +1,13 @@
+package com.folautech.icu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithIcuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWithIcuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/folautech/icu/SpringbootWithIcuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.folautech.icu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithIcuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
